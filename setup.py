dfaf7ec8b2ac6143cbdd0231418df0c7cbf8fb0c
Adeel Ahmad Khan <adeel@adeel.ru>
2011-07-29 23:56:12 -0400

    add Beaker dependency

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
   name="pump",
-  version="0.1.1",
+  version="0.1.2",
   description="A web application library.",
   author="Adeel Ahmad Khan",
   author_email="adeel@adeel.ru",
   packages=["pump", "pump.util", "pump.middleware"],
+  install_requires=["Beaker"],
   license="MIT",
   classifiers=[
     'Topic :: Internet :: WWW/HTTP :: WSGI',
